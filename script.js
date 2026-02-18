156cb9c858675737bf015f06805bd688b11ad690
OwOrawrXDlol <dhhgamer098@gmail.com>
2026-02-18 10:02:31 -0600

    Ajustes SVG + Control de dimensión

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // Obtener la imagen y los botones
 const image = document.getElementById('image');
+const xSmallBtn = document.getElementById('xSmallBtn');
 const smallBtn = document.getElementById('smallBtn');
 const mediumBtn = document.getElementById('mediumBtn');
 const largeBtn = document.getElementById('largeBtn');
+const xLargeBtn = document.getElementById('xLargeBtn');
 
 // Funciones para cambiar el tamaño de la imagen
 smallBtn.addEventListener('click', () => {
@@ -18,4 +20,16 @@ mediumBtn.addEventListener('click', () => {
 largeBtn.addEventListener('click', () => {
     image.style.width = '450px';
     image.style.height = '450px';
+});
+
+// X-Pequeño
+xSmallBtn.addEventListener('click', () => {
+    image.style.width = '75px';
+    image.style.height = '75px';
+});
+
+// X-Grande
+xLargeBtn.addEventListener('click', () => {
+    image.style.width = '600px';
+    image.style.height = '600px';
 });
